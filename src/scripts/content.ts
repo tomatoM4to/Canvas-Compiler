a1fb3958855161736d2b6bbb3ca206582bd3257b
tomato <tomatomato2121@gmail.com>
2023-08-22 22:12:04 +0900

    inject content

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,10 +1,13 @@
 // @ts-ignore
 import paletteTemplate from "@/templates/palette.html";
+// @ts-ignore
+import canvasTemplate from  "@/templates/canvas.html";
+
 import "@/styles/style.css"
 import {dragPalette} from "@/components/dragPalette";
 
 // dom be injected
-const body = document.querySelector('body');
+const body = document.querySelector('body') as HTMLElement;
 
 // palette
 const palette = document.createElement('div');
@@ -14,9 +17,19 @@ palette.innerHTML = paletteTemplate;
 const movingButton = palette.querySelector("#cc-palette-moving-icon") as HTMLElement;
 const paletteContainer = palette.querySelector(".cc-palette-container") as HTMLElement;
 
+const main = document.querySelector('main') as HTMLElement;
+const canvas = document.createElement('div');
+canvas.innerHTML = canvasTemplate;
+
+
 
 if (body) {
     dragPalette(paletteContainer, movingButton);
     body.insertAdjacentElement("afterend", palette);
 }
 
+
+window.onload = () => {
+    main.insertAdjacentElement("afterend", canvas);
+}
+console.log("content injected")
