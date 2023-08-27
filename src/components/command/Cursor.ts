6dca7a7c71f87f95f5a3413c761a8fa5e56e0a24
tomato <tomatomato2121@gmail.com>
2023-08-28 00:30:49 +0900

    create cursor class

---
+++ src/components/command/Cursor.ts
@@ -0,0 +1,26 @@
+import {Command} from "@/components/Toolbar";
+
+export class CursorCommand implements Command {
+    private cursor: Cursor;
+
+    constructor(cursor: Cursor) {
+        this.cursor = cursor;
+    }
+    addEvent() {
+        this.cursor.addEvent();
+    }
+    removeEvent() {
+        this.cursor.removeEvent();
+    }
+}
+
+
+export class Cursor {
+    addEvent() {
+
+    }
+
+    removeEvent() {
+
+    }
+}
