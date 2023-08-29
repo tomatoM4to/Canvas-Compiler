5ffac72ee45301b02b97f3f272d0dbd27526cf8e
tomato <tomatomato2121@gmail.com>
2023-08-29 18:45:35 +0900

    Separated cursor event function.

--- src/components/command/Cursor.ts
+++ src/components/command/Cursor.ts
@@ -36,64 +36,9 @@ export class Cursor {
         konvaSettings.layer.getChildren().forEach(shape => {
             shape.draggable(true);
         });
-
-
-        konvaSettings.stage.on('mousedown touchstart', (e: any) => {
-            if (e.target !== konvaSettings.stage) {
-                return;
-            }
-            e.evt.preventDefault();
-            this.x1 = konvaSettings.stage.getPointerPosition()?.x;
-            this.y1 = konvaSettings.stage.getPointerPosition()?.y;
-            this.x2 = konvaSettings.stage.getPointerPosition()?.x;
-            this.y2 = konvaSettings.stage.getPointerPosition()?.y;
-
-            this.selectionRectangle.visible(true);
-            this.selectionRectangle.width(0);
-            this.selectionRectangle.height(0);
-            console.log(`Cursor: mouse down event`);
-        });
-
-
-        konvaSettings.stage.on('mousemove touchmove', (e: any) => {
-            if (!this.selectionRectangle.visible()) {
-                return;
-            }
-            e.evt.preventDefault();
-            this.x2 = konvaSettings.stage.getPointerPosition()?.x;
-            this.y2 = konvaSettings.stage.getPointerPosition()?.y;
-
-            this.selectionRectangle.setAttrs({
-                // @ts-ignore
-                x: Math.min(this.x1, this.x2),
-                // @ts-ignore
-                y: Math.min(this.y1, this.y2),
-                // @ts-ignore
-                width: Math.abs(this.x2 - this.x1),
-                // @ts-ignore
-                height: Math.abs(this.y2 - this.y1),
-            });
-            console.log(`Cursor: mouse move event`);
-        });
-
-        konvaSettings.stage.on('mouseup touchend', (e: any) => {
-            if (!this.selectionRectangle.visible()) {
-                return;
-            }
-            e.evt.preventDefault();
-            // update visibility in timeout, so we can check it in click event
-            setTimeout(() => {
-                this.selectionRectangle.visible(false);
-            });
-
-            let shapes = konvaSettings.stage.find('.rect');
-            let box = this.selectionRectangle.getClientRect();
-            let selected = shapes.filter((shape: any) =>
-                Konva.Util.haveIntersection(box, shape.getClientRect())
-            );
-            konvaSettings.transfomer.nodes(selected);
-            console.log(`Cursor: mouse up event`);
-        });
+        konvaSettings.stage.on('mousedown touchstart', (e) => this.mouseDown(e));
+        konvaSettings.stage.on('mousemove touchmove', (e) => this.mouseMove(e));
+        konvaSettings.stage.on('mouseup touchend', (e) => this.mouseUp(e));
     }
 
     removeEvent() {
@@ -104,5 +49,58 @@ export class Cursor {
         konvaSettings.stage.off('mousemove touchmove');
         konvaSettings.stage.off('mouseup touchend');
         konvaSettings.transfomer.nodes([]);
+    }
+
+    private mouseDown(e: any) {
+        if (e.target !== konvaSettings.stage) {
+            return;
+        }
+        e.evt.preventDefault();
+        this.x1 = konvaSettings.stage.getPointerPosition()?.x;
+        this.y1 = konvaSettings.stage.getPointerPosition()?.y;
+        this.x2 = konvaSettings.stage.getPointerPosition()?.x;
+        this.y2 = konvaSettings.stage.getPointerPosition()?.y;
+
+        this.selectionRectangle.visible(true);
+        this.selectionRectangle.width(0);
+        this.selectionRectangle.height(0);
+    }
+
+    private mouseMove(e: any) {
+        if (!this.selectionRectangle.visible()) {
+            return;
+        }
+        e.evt.preventDefault();
+        this.x2 = konvaSettings.stage.getPointerPosition()?.x;
+        this.y2 = konvaSettings.stage.getPointerPosition()?.y;
+
+        this.selectionRectangle.setAttrs({
+            // @ts-ignore
+            x: Math.min(this.x1, this.x2),
+            // @ts-ignore
+            y: Math.min(this.y1, this.y2),
+            // @ts-ignore
+            width: Math.abs(this.x2 - this.x1),
+            // @ts-ignore
+            height: Math.abs(this.y2 - this.y1),
+        });
+    }
+
+    private mouseUp(e: any) {
+        if (!this.selectionRectangle.visible()) {
+            return;
+        }
+        e.evt.preventDefault();
+        // update visibility in timeout, so we can check it in click event
+        setTimeout(() => {
+            this.selectionRectangle.visible(false);
+        });
+
+        let shapes = konvaSettings.stage.find('.rect');
+        let box = this.selectionRectangle.getClientRect();
+        let selected = shapes.filter((shape: any) =>
+            Konva.Util.haveIntersection(box, shape.getClientRect())
+        );
+        konvaSettings.transfomer.nodes(selected);
     }
 }
